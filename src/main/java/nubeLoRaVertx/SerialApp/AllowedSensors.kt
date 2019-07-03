e454a1fe8488591ff958235360263a5bdc0800e1
NubeDev <aidan@enviroservices.com.au>
2019-07-03 15:09:03 +1000

    Added an example

--- src/main/java/nubeLoRaVertx/SerialApp/AllowedSensors.kt
+++ src/main/java/nubeLoRaVertx/SerialApp/AllowedSensors.kt
@@ -1,36 +1,21 @@
 package nubeLoRaVertx.SerialApp
 
-
 import Response.getAllSensors.sensorQueryAll
 import db.connector
-
-
 import org.json.JSONArray
 import org.json.JSONObject
 
 
-import io.vertx.core.Vertx
-import io.vertx.core.json.JsonArray
-import io.vertx.ext.web.client.WebClient
-import io.vertx.kotlin.core.json.json
-import io.vertx.kotlin.core.json.obj
-
-
 fun main(args: Array<String>) {
 
-
     /*
-
-
    Steps of adding a new sensor See the example vertxWebClientAddSensor.kt
    1) A user would add a new sensor from the bs-api-dashboard. this would be stored in the SQLLite DB
-
 
    Steps of sending data to bs-api
    1) Stream in serail Data from an arduino
    2) deycyped the data and see if the nodeId is save in the DB
    3) If data is save in the DB then post the sensor data to bs-api
-
 
    */
 
@@ -45,11 +30,8 @@ fun main(args: Array<String>) {
     val serialDataExample2 = "ABB296C4E5094228BA0000EC0000009A2D64"
 //    val nodeId = serialDataExample.substring(0, 8)
 
-
     val dataIn = serialDataExample
     val nodeId = dataIn.substring(0, 8) //AAB296C4
-
-
 
     // Call DB for sensor that are added to the DB
     val manager = sensorQueryAll(connector)
@@ -66,8 +48,6 @@ fun main(args: Array<String>) {
     }.map {
         it.getString("nodeId")
     }
-
-
 
     //check if nodeId is in the DB
     fun vaildNodeId(sensorList :List<String>, msgNodeID :String, rawMsg :String ): JSONArray {
@@ -99,14 +79,12 @@ fun main(args: Array<String>) {
             val snrRaw = subOne(rawMsg, 34, 36) //10
             val snr = snrRaw.toLong(16).toFloat() / 10
 
-
             val tempPayload = mutableMapOf("id" to "${nodeId}_TEMP", "value" to temp, "priority" to 16)
             val humidityPayload = mutableMapOf("id" to "${nodeId}_HUMIDITY", "value" to humidity, "priority" to 16)
             val movementPayload  = mutableMapOf("id" to "${nodeId}_MOVEMENT", "value" to movement, "priority" to 16)
             val luxPayload  = mutableMapOf("id" to "${nodeId}_LUX", "value" to lux, "priority" to 16)
             val voltagePayload  = mutableMapOf("id" to "${nodeId}_VOLTAGE", "value" to voltage, "priority" to 16)
             val rssiPayload  = mutableMapOf("id" to "${nodeId}_RSSI", "value" to rssi, "priority" to 16)
-
 
             val sensorPayload = JSONArray()
             sensorPayload.put(tempPayload)
@@ -131,21 +109,9 @@ fun main(args: Array<String>) {
 
 
 
-
-
     // send data to bs-api via a vertx web-cleint http post
     val postBsAPI = vaildNodeId(filterNodeIds, nodeId, dataIn)
     postBsAPI.forEach({a -> println(a)})
-
-
-
-
-
-
-
-
-
-
 
 
     //pos data to bs-api
